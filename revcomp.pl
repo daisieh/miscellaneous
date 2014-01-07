14ec236331f663ea821200e2cfd07eb8b6af2946
daisieh_local <daisieh@gmail.com>
2014-01-06 19:39:54 -0800

    rewrite parse_fasta

--- revcomp.pl
+++ revcomp.pl
@@ -2,8 +2,18 @@
 
 use strict;
 
-my $seq = shift @ARGV;
-print reverse_complement($seq) . "\n";
+my $input = shift @ARGV;
+
+if ($input =~ /\.fa/) {
+	#input is a fasta file
+	my ($taxa, $taxnames) = parse_fasta($input);
+	foreach my $taxon (@$taxnames) {
+		print ">$taxon\n";
+		print reverse_complement($taxa->{$taxon}) . "\n";
+	}
+} else {
+	print reverse_complement($input) . "\n";
+}
 
 sub reverse_complement {
 	my $charstr = shift;
@@ -14,4 +24,36 @@ sub reverse_complement {
 	# complement the reversed DNA sequence
 	$revcomp =~ tr/ABCDGHMNRSTUVWXYabcdghmnrstuvwxy/TVGHCDKNYSAABWXRtvghcdknysaabwxr/;
 	return $revcomp;
+}
+
+sub parse_fasta {
+	my $inputfile = shift;
+
+	my $taxa = {};
+	my @taxanames = ();
+	open (fileIN, "$inputfile") or die "no file named $inputfile";
+
+	my $input = readline fileIN;
+	my $length = 0;
+	my $taxonlabel = "";
+	my $sequence = "";
+	while ($input ne "") {
+		if ($input =~ /^>(.+)\s*$/) {
+			$taxonlabel = $1;
+			push @taxanames, $taxonlabel;
+			if ($length > 0) {
+				# we are at the next taxon; push the last one onto the taxon array.
+				$taxa->{"length"} = $length;
+				$length = 0;
+			}
+		} else {
+			$input =~ /^\s*(.+)\s*$/;
+			$taxa->{$taxonlabel} .= $1;
+			$length += length($1);
+		}
+		$input = readline fileIN;
+	}
+
+	close (fileIN);
+	return $taxa, \@taxanames;
 }
